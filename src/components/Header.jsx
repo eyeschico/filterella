3986eb0cbddb29636c4a83c60250079d5b2011f1
Mohammed Khalef <eyeschico@mohammedkhalef.com>
2022-08-28 14:03:33 +0200

    setup project

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Filterella</h1>
+    </div>
+  );
+};
+
+export default Header;

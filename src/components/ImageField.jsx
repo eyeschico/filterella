f3e4188e73fc4d95851954f3e77bfbaaa94517c0
Mohammed Khalef <eyeschico@mohammedkhalef.com>
2022-08-31 22:50:50 +0200

    Add domToImage and file-saver for download render, minor(add styled from mui)

--- src/components/ImageField.jsx
+++ src/components/ImageField.jsx
@@ -1,9 +1,17 @@
-import { useRef, useState } from "react";
+import { useContext, useRef, useState } from "react";
+import { FilterContext } from "../App";
 import "./ImageField.css";
+import { styled } from "@mui/system";
+import domToImage from "dom-to-image";
+import { saveAs } from "file-saver";
 
 const ImageField = () => {
   const uploadInputRef = useRef(null);
+  const imgResultRef = useRef(null);
   const [imageFile, setImageFile] = useState(null);
+  const { filterClass, customFilter } = useContext(FilterContext);
+
+  //console.log(filterClass);
 
   const uploadRefWindow = () => {
     uploadInputRef.current && uploadInputRef.current.click();
@@ -13,16 +21,37 @@ const ImageField = () => {
     setImageFile(URL.createObjectURL(event.target.files[0]));
   };
 
-  const renderImage = () => (
-    <img style={styledImage} src={imageFile} alt="Current image" />
-  );
+  const handleDownloadImage = () => {
+    //console.log(imgResultRef);
+    domToImage
+      .toBlob(imgResultRef.current)
+      .then((blob) => {
+        saveAs(blob, "filterella.png");
+      })
+      .catch((error) => {
+        console.error("Something went wrong", error);
+      });
+  };
 
-  const styledImage = {
+  const StyledImage = styled("img")((props) => ({
     width: "100%",
     height: "100%",
     objectFit: "contain",
     boxShadow: "0px 0px 10px #000000",
-  };
+    filter: `contrast(${props.customFilter.contrast}%) brightness(${props.customFilter.brightness}%) saturate(${props.customFilter.saturate}%) sepia(${props.customFilter.sepia}%) grayScale(${props.customFilter.gray}%)`,
+  }));
+
+  const renderImage = () => (
+    <figure>
+      <StyledImage
+        customFilter={!filterClass && customFilter}
+        className={filterClass}
+        src={imageFile}
+        ref={imgResultRef}
+        alt="Current Image"
+      />
+    </figure>
+  );
 
   return (
     <div className="image-container">
@@ -42,7 +71,10 @@ const ImageField = () => {
         accept="image/*"
         hidden
       />
-      <button className={`button ${!imageFile ? "disabled" : ""}`}>
+      <button
+        onClick={handleDownloadImage}
+        className={`button ${!imageFile ? "disabled" : ""}`}
+      >
         Downlod Image
       </button>
     </div>

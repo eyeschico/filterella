4610df98b5dd1b35358a32dccaefb7ee079598e5
Mohammed Khalef <eyeschico@mohammedkhalef.com>
2022-08-31 22:46:59 +0200

    set context(for filter and custom filter)

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,23 @@ export const FilterContext = createContext();
 function App() {
   const [tabFilter, setTabFilter] = useState("instaFilter");
   const [filterClass, setFilterClass] = useState("");
+  const [customFilter, setCustomFilter] = useState({
+    contrast: 100,
+    brightness: 100,
+    saturate: 100,
+    sepia: 0,
+    gray: 0,
+  });
+
+  //console.log(customFilter);
 
   const value = {
     tabFilter,
     setTabFilter,
     filterClass,
     setFilterClass,
+    customFilter,
+    setCustomFilter,
   };
 
   return (

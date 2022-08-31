3e2d7b78ef2097c51d1f191b116c33c3fb16d885
Mohammed Khalef <eyeschico@mohammedkhalef.com>
2022-08-31 22:44:26 +0200

    InstaFilter(set filter with useContext)

--- src/components/InstaFilter.jsx
+++ src/components/InstaFilter.jsx
@@ -3,15 +3,22 @@ import { filterValues } from "../utils";
 import { FilterContext } from "../App";
 import instaPic from "../assets/instagram.jpg";
 import styles from "./InstaFilter.module.css";
+import "../App.css";
 
-console.log(instaPic);
+//console.log(instaPic);
 const InstaFilter = () => {
   const { filterClass, setFilterClass } = useContext(FilterContext);
+
+  const handleChange = (event) => {
+    setFilterClass(event.target.value);
+  };
+
   return (
     <div className={`${styles["insta-box"]}`}>
       {filterValues.map((filter) => (
         <figure
-          value={filter.class}
+          value={filterClass}
+          label="Filter"
           key={filter.class}
           className={`${styles["insta-filter"]}`}
         >
@@ -20,7 +27,13 @@ const InstaFilter = () => {
             alt="Render"
             className={`${styles["insta-pic"]} ${filter.class}`}
           />
-          <button className={`${styles["insta-name"]}`}>{filter.name}</button>
+          <button
+            onClick={handleChange}
+            value={filter.class}
+            className={`${styles["insta-name"] && "button"}`}
+          >
+            {filter.name}
+          </button>
         </figure>
       ))}
     </div>
